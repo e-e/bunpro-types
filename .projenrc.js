89349c18ab1bde5b6027cfec65f17e36f2806434
Eric Ellingson <god@eric.wtf>
2022-01-13 17:22:55 -0600

    private npm repo stuff

--- .projenrc.js
+++ .projenrc.js
@@ -5,10 +5,25 @@ const project = new cdk.JsiiProject({
   defaultReleaseBranch: 'main',
   name: 'bunpro-types',
   repositoryUrl: 'https://github.com/god/bunpro-types.git',
+  npmRegistryUrl: 'https://npm.pkg.github.com',
+  workflowBootstrapSteps: [
+    {
+      name: 'Authenticate to private GitHub npm registry',
+      run: `cat > .npmrc <<EOF
+//npm.pkg.github.com/:_authToken=\${{ secrets.PRIVATE_NPM_REGISTRY_TOKEN }}
+@e-e:registry=https://npm.pkg.github.com/
+EOF
+`,
+    },
+  ],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.addGitIgnore('.npmrc');
+
+
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,67 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
-  }
-}
+type JapaneseTargetSentence = {
+  id: number;
+  text: string;
+  html: string;
+  target: string;
+};
+
+type JapaneseExampleSentence = {
+  id: number;
+  text: string;
+  target: string;
+  html: string;
+};
+
+type EnglishExampleSentence = {
+  id: number;
+  text: string;
+  target: string;
+  html: string;
+};
+
+type ExampleSentence = {
+  japanese: JapaneseExampleSentence;
+  english: EnglishExampleSentence;
+};
+
+type JLPTLevel = 'N1' | 'N2' | 'N3' | 'N4' | 'N5';
+
+type GrammarNumber = {
+  number: number;
+  total: number;
+};
+
+type GrammarInfo = {
+  id: number;
+  level: JLPTLevel;
+  lesson: string;
+  grammar: GrammarNumber;
+};
+
+type EnglishTargetSentence = string;
+type StudyTarget = string;
+
+type RelatedGrammars = {
+  synonyms: RelatedGrammar[];
+  antonyms: RelatedGrammar[];
+};
+
+type RelatedGrammar = {
+  grammar: string;
+  url: string;
+};
+
+type TargetSentence = {
+  japanese: JapaneseTargetSentence;
+  english: EnglishTargetSentence;
+  target: StudyTarget;
+};
+
+type ReviewData = {
+  grammar: GrammarInfo;
+  sentence: TargetSentence;
+  examples: ExampleSentence[];
+  related: RelatedGrammars;
+};
+
+type RelatedGrammarType = 'antonyms' | 'synonyms';
